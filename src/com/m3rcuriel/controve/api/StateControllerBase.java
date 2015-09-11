3f43375f13b00aa22ebe31d4778c3df5d87dcfe3
Lee Mracek <lee.mracek@gmail.com>
2015-09-10 22:57:47 -0700

    Move Controve files from rewrite project

---
+++ src/com/m3rcuriel/controve/api/StateControllerBase.java
@@ -0,0 +1,39 @@
+package com.m3rcuriel.controve.api;
+
+import com.m3rcuriel.controve.retrievable.Retrievable;
+import com.m3rcuriel.controve.retrievable.StateHolder;
+
+public abstract class StateControllerBase<T extends CommandsBase, K extends RobotSetpointsBase> implements Retrievable {
+
+  protected Routine<T, K> currentRoutine = null;
+  protected RobotSetpointsBase setpoints;
+
+  private void setNewRoutine(Routine<T, K> newRoutine) {
+    boolean needsCancel = newRoutine != currentRoutine && currentRoutine != null;
+
+    boolean needsReset = newRoutine != currentRoutine && newRoutine != null;
+    if (needsCancel) {
+      currentRoutine.cancel();
+    }
+    currentRoutine = newRoutine;
+    if (needsReset) {
+      currentRoutine.reset();
+    }
+  }
+
+  public void reset() {
+    setNewRoutine(null);
+  }
+
+  public abstract void update(CommandsBase commands);
+
+  @Override
+  public void getState(StateHolder states) {
+    states.put("mode", currentRoutine != null ? currentRoutine.getName() : "---");
+  }
+
+  @Override
+  public String getName() {
+    return "state controller";
+  }
+}
---
+++ src/com/m3rcuriel/controve/retrievable/SystemManager.java
@@ -0,0 +1,106 @@
+package com.m3rcuriel.controve.retrievable;
+
+import org.json.simple.JSONObject;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+public class SystemManager {
+  private static SystemManager inst = null;
+
+  private class RetrievableHolder {
+    Retrievable machine;
+    StateHolder stateHolder = new StateHolder();
+
+    public RetrievableHolder(Retrievable retrievable) {
+      machine = retrievable;
+    }
+
+    public StateHolder getStateHolder() {
+      return stateHolder;
+    }
+
+    public void update() {
+      machine.getState(stateHolder);
+    }
+  }
+
+  private HashMap<String, RetrievableHolder> retrievables;
+
+  public static SystemManager getInstance() {
+    if (inst == null) {
+      inst = new SystemManager();
+    }
+    return inst;
+  }
+
+  public SystemManager() {
+    this.retrievables = new HashMap<String, RetrievableHolder>();
+  }
+
+  public void add(Retrievable retrievable) {
+    RetrievableHolder retrievableHolder = new RetrievableHolder(retrievable);
+    retrievables.put(retrievable.getName(), retrievableHolder);
+  }
+
+  public void add(Collection<Retrievable> values) {
+    for (Retrievable retrievable : values) {
+      RetrievableHolder smh = new RetrievableHolder(retrievable);
+      retrievables.put(retrievable.getName(), smh);
+    }
+  }
+
+  private void updateStates(String systemKey) {
+    retrievables.get(systemKey).update();
+  }
+
+  private void updateAllStates() {
+    retrievables.forEach((key, rh) -> rh.update());
+  }
+
+  private Object getValueForKey(String k) {
+    String[] pieces = k.split(Pattern.quote("."));
+    if (pieces.length != 2) {
+      return null;
+    }
+    String base = pieces[0];
+    String key = pieces[1];
+
+    RetrievableHolder retrievableHolder = retrievables.get(base);
+    if (retrievableHolder == null) {
+      return null;
+    }
+
+    StateHolder sh = retrievableHolder.getStateHolder();
+
+    if (sh == null) {
+      return null;
+    }
+
+    return sh.get(key);
+  }
+
+  public JSONObject get() {
+    JSONObject states = new JSONObject();
+
+    updateAllStates();
+
+    retrievables.forEach((key, rh) -> rh.getStateHolder().forEach(
+        (entry) -> states.put(key + "." + entry.getKey(), entry.getValue())));
+    return states;
+  }
+
+  public JSONObject get(String k) {
+    return get(new String[] {k});
+  }
+
+  public JSONObject get(String[] args) {
+    updateAllStates();
+    JSONObject states = new JSONObject();
+    for (String k : args) {
+      states.put(k, getValueForKey(k));
+    }
+    return states;
+  }
+}
