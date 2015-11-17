a55dd010835ebeaf365f2e83857065801e5da0b2
Lee Mracek <lee.mracek@gmail.com>
2015-11-17 12:40:25 -0800

    Incorperate checking in CSVWriter
    Verify that number of arguments provided through
    writeline is equal to the arguments for the header
    data

--- src/com/m3rcuriel/controve/util/CSVWriter.java
+++ src/com/m3rcuriel/controve/util/CSVWriter.java
@@ -5,21 +5,26 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class CSVWriter extends FileWriter {
+	
+	int index;
 
 	public CSVWriter(File file, String... headerData) throws IOException {
 		super(file);
 		if (headerData.length != 0) {
+			index = headerData.length;
 			StringBuilder head = new StringBuilder();
 			for (String header : headerData) {
 				head.append(header + ",");
 			}
 			super.write(head.substring(0, head.length() - 1) + "\n");
 			super.flush();
+		} else {
+			index = -1;
 		}
 	}
 
 	public void writeLine(Number... data) {
-		if (data.length != 0) {
+		if (data.length != 0 && (data.length == index || index == -1)) {
 			StringBuilder head = new StringBuilder();
 			for (Number header : data) {
 				head.append(header + ",");
@@ -30,6 +35,8 @@ public class CSVWriter extends FileWriter {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		} else {
+			throw new IllegalArgumentException("Number of parameters must match CSV data");
 		}
 	}
 }
