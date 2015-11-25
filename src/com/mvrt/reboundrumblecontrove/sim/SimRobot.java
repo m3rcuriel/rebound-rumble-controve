619c79856de99225dd0ac2828e7c6e8d29caa885
Lee Mracek <lee.mracek@gmail.com>
2015-11-25 12:25:16 -0800

    Add target to build.xml

---
+++ src/com/mvrt/reboundrumblecontrove/sim/SimRobot.java
@@ -0,0 +1,16 @@
+package com.mvrt.reboundrumblecontrove.sim;
+
+import com.team254.fakewpilib.SimRobotBase;
+
+public class SimRobot extends SimRobotBase {
+
+  @Override
+  public void initSimRobot() {
+  }
+
+  @Override
+  public void startRobotSim() {
+    // no hardware simulation for now
+  }
+
+}
